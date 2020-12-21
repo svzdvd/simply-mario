d774b9a7107e29145afa8092293d57f6354b1232
Davide Savazzi <davide.savazzi@gmail.com>
2020-12-21 12:39:56 +0000

    Backgrounds & Tiles

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+import SpriteSheet from './SpriteSheet.js';
+import {loadImage, loadLevel} from './loaders.js';
+
+function drawBackground(background, context, sprites) {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        for (let x = x1; x < x2; ++x) {
+            for (let y = y1; y < y2; ++y) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        }
+    });
+}
+
+function loadBackgroundSprites() {
+    return loadImage('img/tiles.png')
+    .then(image => {
+        const sprites = new SpriteSheet(image, 16, 16);
+        sprites.define('ground', 0, 0);
+        sprites.define('sky', 10, 7);
+        return sprites;
+    });
+}
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+Promise.all([
+    loadBackgroundSprites(),
+    loadLevel('1-1')
+])
+.then(([sprites, level]) => {
+    loadLevel('1-1')
+    .then(level => {
+        level.backgrounds.forEach(b => drawBackground(b, context, sprites));
+    });
+});
